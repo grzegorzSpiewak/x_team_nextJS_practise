3acdc062f911cdf4be813509862251f28cd1c853
Josh Johnston <josh@x-team.com>
2017-05-11 15:01:16 +1000

    (wip) example of adding static css

--- templates/Layout.js
+++ templates/Layout.js
@@ -1,11 +1,16 @@
 'use strict'
 import React from 'react'
+import Head from 'next/head'
 import Menu from '../components/Menu/Menu'
 import Footer from '../components/Footer/Footer'
 import Homepage from '../data/Homepage'
 
 const Layout = ({ children }, props) => (
   <div>
+    <Head>
+      <title>X-Team</title>
+      <link rel='stylesheet' href='/static/css/hero.css' />
+    </Head>
     <Menu data={ Homepage.Menu.Items } />
     {children}
     <Footer />
